bd336159fbce6da32671dad844c23cad2ab8ee28
kimkimgungunwoo <gunwoo7179@gmail.com>
2026-09-02 17:49:31 +0900

    CD수정

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 from bot.core.bot import create_bot
 from dotenv import load_dotenv
+import asyncio
 import os
 import logging
 
@@ -10,6 +11,9 @@ logging.getLogger("watchfiles").setLevel(logging.WARNING)
 
 load_dotenv()
 token = os.getenv("token")
+
+from scripts.init_dynamodb import main as init_dynamodb
+asyncio.run(init_dynamodb())
 
 bot = create_bot()
 bot.run(token)
